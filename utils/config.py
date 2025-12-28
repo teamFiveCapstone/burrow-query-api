c5101bb457e4eb040a20bc4add505c7a66199d73
teamFiveCapstone <Bajcarzach@icloud.com>
2025-12-27 19:23:17 -0500

    swapped model to tital text express

--- utils/config.py
+++ utils/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
 
     aws_region: str = "us-east-1"
     bedrock_model_id: str = "amazon.titan-embed-text-v2:0"
-    bedrock_llm_model_id: str = "amazon.titan-text-premier-v1:0"
+    bedrock_llm_model_id: str = "amazon.titan-text-express-v1"
 
     api_title: str = "Burrow Query API"
     api_version: str = "1.0.0"
